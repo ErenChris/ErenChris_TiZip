60d3916ff76ed19a4d60e87f13c8ab57815a7e84
ErenChris <2485552681@qq.com>
2021-05-17 20:43:55 +0800

    Update

--- TiZip/Form1.Designer.cs
+++ TiZip/Form1.Designer.cs
@@ -31,8 +31,8 @@ namespace TiZip
         {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(600, 300);
+            this.Text = "TiZip";
         }
 
         #endregion
